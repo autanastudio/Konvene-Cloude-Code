5648b9f514f6ca12b4d6ffab7dad4b8fa4f633a5
Alexey Belezeko <alexey.belezeko@sfcd.com>
2015-03-31 23:27:01 +0300

    Add follow mechanizm

--- main.js
+++ main.js
@@ -41,3 +41,79 @@ Parse.Cloud.define("checkUsersFromContacts", function(request, response) {
     }
   });
 });
+
+Parse.Cloud.beforeSave("FollowAction", function(request, response) {
+  var fromUser = request.object.get("from");
+  var toUser = request.object.get("to");
+  var sender = request.user;
+  if (fromUser.id === toUser.id) {
+    response.error(JSON.stringify({code: 105, message: "You cannot follow yourself!"}));
+  }
+  if (fromUser.id === sender.id) {
+    var actionQuery = new Parse.Query("FollowAction");
+    actionQuery.equalTo("from", fromUser);
+    actionQuery.equalTo("to", toUser);
+    actionQuery.first({
+      success: function(action) {
+        if(action) {
+          response.error(JSON.stringify({code: 108, message: "You alredy follows this user"}));
+        } else {
+          response.success();
+        }
+      },
+      error: function(error) {
+        console.log("error: "+error.code+" "+error.message);
+        response.error(JSON.stringify({code: 107, message: "Action search error", error: error}));
+      }
+    });
+  } else {
+    response.error(JSON.stringify({code: 106, message: "You doesn't have permissions for that action!"}));
+  }
+});
+
+Parse.Cloud.beforeDelete("FollowAction", function(request, response) {
+  var fromUser = request.object.get("from");
+  var toUser = request.object.get("to");
+  var sender = request.user;
+  if (fromUser.id === sender.id) {
+    response.success();
+  } else {
+    response.error(JSON.stringify({code: 106, message: "You doesn't have permissions for that action!"}));
+  }
+});
+
+Parse.Cloud.afterSave("FollowAction", function(request) {
+  var fromUser = request.object.get("from");
+  var toUser = request.object.get("to");
+  updateUserCount(fromUser, "from", "followingCount");
+  updateUserCount(toUser, "to", "followerCount");
+});
+
+Parse.Cloud.afterDelete("FollowAction", function(request) {
+  var fromUser = request.object.get("from");
+  var toUser = request.object.get("to");
+  updateUserCount(fromUser, "from", "followingCount");
+  updateUserCount(toUser, "to", "followerCount");
+});
+
+var updateUserCount = function(user, userKey, countKey) {
+  var followersQuery = new Parse.Query("FollowAction");
+  followersQuery.equalTo(userKey, user);
+  followersQuery.find({
+    success: function (results) {
+      user.set(countKey, results.length);
+      user.save(null, {
+        useMasterKey: true,
+        success: function() {
+          console.log("User save ok");
+        },
+        error: function(cs, error) {
+          console.log("User save error: "+error.code+" "+error.message);
+        }
+      });
+    },
+    error: function (results, error) {
+      console.log(error.message);
+    }
+  });
+};
